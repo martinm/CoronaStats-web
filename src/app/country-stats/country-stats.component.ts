cbef8d508ca1a20e267fedabf4a84e9a9679bb2a
martinm <malinowskimn@gmail.com>
2020-08-14 21:43:36 +0100

    Update styling

--- src/app/country-stats/country-stats.component.ts
+++ src/app/country-stats/country-stats.component.ts
@@ -10,7 +10,6 @@ import { CountryService } from '../country.service';
 })
 export class CountryStatsComponent implements OnInit {
   countries: Country[];
-  switch = true;
 
   constructor(
     private countryService: CountryService
@@ -25,24 +24,8 @@ export class CountryStatsComponent implements OnInit {
       stats => this.countries = stats)
   }
 
-  testing(prop): void {
-    if (this.switch) {
-      this.countries.sort((a, b) => (a[prop] > b[prop]) ? 1 : -1)
-    } else {
+  sort(prop): void {
       this.countries.sort((a, b) => (a[prop] < b[prop]) ? 1 : -1)
-    }
-    this.switch = !this.switch
   }
-
-  compare( a, b ) {
-    if ( a.cases > b.cases ){
-      return -1;
-    }
-    if ( a.cases < b.cases ){
-      return 1;
-    }
-    return 0;
-  }
-
   
 }
